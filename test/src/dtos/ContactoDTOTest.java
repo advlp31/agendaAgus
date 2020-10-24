d24c51046dc9093a5ca8852fec19615537762bd4
advlp31 <agustinaguirre31@gmail.com>
2020-10-24 13:02:52 -0300

    Cree scriptdb #2

--- test/src/dtos/ContactoDTOTest.java
+++ test/src/dtos/ContactoDTOTest.java
@@ -5,12 +5,11 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class ContactoDTOTest {
-	
+
 	private static final String EMAIL = "test@mail.com";
-	
+
 	private ContactoDTO dto = new ContactoDTO(EMAIL, 441122);
-	
-	
+
 	@Test
 	public void testContactoDTO() {
 		assertNotNull(dto);
@@ -20,7 +19,7 @@ public class ContactoDTOTest {
 	public void testGetEmail() {
 		assertNotNull(dto.getEmail());
 	}
-	
+
 	@Test
 	public void testGetEmailNotFails() {
 		assertEquals(dto.getEmail(), EMAIL);
--- test/src/dtos/PersonaDTOTest.java
+++ test/src/dtos/PersonaDTOTest.java
@@ -7,53 +7,49 @@ import java.util.Date;
 import org.junit.Test;
 
 public class PersonaDTOTest {
-	
+
 	private static String nombreCompleto = "Nombre Prueba";
-	
-	private static Date fechaNacimiento = new Date (1999,03,01);
-	
+
+	private static Date fechaNacimiento = new Date(1999, 03, 01);
+
 	private DomicilioDTO domicilio = new DomicilioDTO("Avenida Peron", 2738);
-	
+
 	private ContactoDTO contacto = new ContactoDTO("email@test.com", 12312);
-	
-	private PersonaDTO persona = new PersonaDTO(nombreCompleto, contacto); 
-	
-	
-	
+
+	private PersonaDTO persona = new PersonaDTO(nombreCompleto, contacto);
+
 	@Test
 	public void testPersonaDTO() {
 		assertNotNull(persona);
 	}
-	
+
 	@Test
 	public void testGetNombreCompleto() {
 		assertEquals(nombreCompleto, persona.getNombreCompleto());
 	}
-	
+
 	@Test
 	public void testGetContacto() {
 		persona.contacto(contacto);
 		assertEquals(contacto, persona.getContacto());
-	}	
-	
+	}
+
 	@Test
 	public void testGetDomicilio() {
 		persona.domicilio(domicilio);
 		assertEquals(domicilio, persona.getDomicilio());
 	}
-	
+
 	@Test
 	public void testGetGenero() {
 		persona.genero(Genero.HOMBRE);
 		assertEquals(Genero.HOMBRE, persona.getGenero());
 	}
-	
+
 	@Test
 	public void testGetFechaNacimiento() {
 		persona.fechaNacimiento(fechaNacimiento);
 		assertEquals(fechaNacimiento, persona.getFechaNacimiento());
 	}
-	
-	
 
 }
